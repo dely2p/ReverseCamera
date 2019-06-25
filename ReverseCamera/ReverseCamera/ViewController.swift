eb32873bce47f2627beabea7d04d8825ecd8bd08
dely2p <lhyal2p@gmail.com>
2019-06-26 02:32:18 +0900

    Add capture button to take photo

--- ReverseCamera/ReverseCamera/ViewController.swift
+++ ReverseCamera/ReverseCamera/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var capturePreviewView: UIView!
+    @IBOutlet weak var captureButton: UIButton!
+    let cameraController = CameraController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureCameraController()
+        makeCaptureButton()
     }
 
-
+    func configureCameraController() {
+        cameraController.prepare {(error) in
+            if let error = error {
+                print(error)
+            }
+            
+            try? self.cameraController.displayPreview(on: self.capturePreviewView)
+        }
+    }
+    
+    func makeCaptureButton() {
+        captureButton.layer.borderColor = UIColor.black.cgColor
+        captureButton.layer.borderWidth = 2
+        
+        captureButton.layer.cornerRadius = min(captureButton.frame.width, captureButton.frame.height) / 2
+    }
 }
 
